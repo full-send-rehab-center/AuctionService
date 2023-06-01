dbfad1bc58ffe3c0b45730df31290bf2b9b0586d
Sebastian Jensen <sohjau@gmail.com>
2023-06-01 22:13:42 +0200

    Fixed Mikkels bullshit

--- auctionServiceAPI/Controllers/AuctionServiceController.cs
+++ auctionServiceAPI/Controllers/AuctionServiceController.cs
@@ -24,12 +24,10 @@ public class AuctionController : ControllerBase
     private readonly IConfiguration _config;
     private readonly IMongoDatabase _database;
     private readonly ILogger<AuctionController> _logger;
-    private readonly string? _docPath;
     private readonly string? _rabbitMQ;
     public IConnectionFactory ConnectionFactory { get; set; }
     public IMongoDatabase Database { get; set; }
     public IMongoCollection<Auction> AuctionCollection { get; set; }
-    public IMongoCollection<UserDTO> UsersCollection { get; set; }
     public IMongoCollection<BidDTO> BidCollection { get; set; }
     private List<Auction> _auctions = new List<Auction>();
     private List<UserDTO> _users = new List<UserDTO>();
@@ -38,7 +36,7 @@ public class AuctionController : ControllerBase
     private readonly HttpClient _httpClientProduct;
     private readonly string? _baseURIUser;
     private readonly string? _baseURIProduct;
- 
+
     public AuctionController(ILogger<AuctionController> logger, IConfiguration config, HttpClient httpClientUser, HttpClient httpClientProduct)
     {
         // Initiates HttpClient for UserService
@@ -52,25 +50,24 @@ public class AuctionController : ControllerBase
         //Takes enviroment variable and sets it to the logger
         _logger = logger;
         _config = config;
-        _docPath = config["DocPath"];
         _rabbitMQ = config["RabbitMQ"];
-        
 
         //Retrieves host name and IP address from the current enviroment
         var hostName = System.Net.Dns.GetHostName();
         var ips = System.Net.Dns.GetHostAddresses(hostName);
         var _ipaddress = ips.First().MapToIPv4().ToString();
 
-        //Logs the enviroment variable
-        _logger.LogInformation($"File path is set to : {_docPath}");
-        _logger.LogInformation($"RabbitMQ connection is set to : {_rabbitMQ}");
-
-        //Connects to the database
+                //Connects to the database
         var client = new MongoClient(_config["ConnectionString"]);
         _database = client.GetDatabase(_config["DatabaseName"]);
         AuctionCollection = _database.GetCollection<Auction>(_config["CollectionName"]);
-        UsersCollection = _database.GetCollection<UserDTO>(_config["MongoDB:UsersCollection"]);
-        BidCollection = _database.GetCollection<BidDTO>(_config["MongoDB:BidCollection"]);
+        BidCollection = _database.GetCollection<BidDTO>(_config["BidCollection"]);
+
+        //Logs the enviroment variable
+        _logger.LogInformation($"RabbitMQ connection is set to : {_rabbitMQ}");
+        _logger.LogInformation($"ConnectionString connection is set to : {_config["ConnectionString"]}");
+        _logger.LogInformation($"DatabaseName connection is set to : {_config["DatabaseName"]}");
+        _logger.LogInformation($"CollectionName connection is set to : {_config["CollectionName"]}");
     }
 
     // HttpClient that pulls a user by ID from UserService
@@ -90,22 +87,8 @@ public class AuctionController : ControllerBase
     {
         _httpClientProduct.BaseAddress = new Uri(_baseURIProduct);
         var produkt = await _httpClientProduct.GetFromJsonAsync<ProduktKatalog>(
-                    $"api/produktkatalog/category/{id}");
+                    $"api/produktkatalog/products/{id}");
         return produkt;
-    }
-
-
-    private IMongoCollection<UserDTO> GetUsersCollection(IConfiguration config)
-    {
-        var ConnectionString = config["MongoDB:ConnectionString"];
-        var database = config["MongoDB:Database"];
-        var usersCollection = config["MongoDB:UsersCollection"];
-
-        var client = new MongoClient(ConnectionString);
-        var db = client.GetDatabase(database);
-        var collection = db.GetCollection<UserDTO>(usersCollection);
-
-        return collection;
     }
 
     [HttpGet("auctions", Name = "GetAuctions")]
@@ -118,7 +101,7 @@ public class AuctionController : ControllerBase
         return auctionCollection.ToList();
     }
 
-     [HttpPost("auction", Name = "CreateAuction")]
+    [HttpPost("auction", Name = "CreateAuction")]
     public async Task<IActionResult> CreateAuction([FromBody] Auction auction, [FromQuery] string userid, [FromQuery] string produktid)
     {
         // UserDTO user = UsersCollection.Find(x => x.UserId == id).FirstOrDefault();
@@ -136,110 +119,35 @@ public class AuctionController : ControllerBase
         AuctionCollection.InsertOne(auction);
         return Ok();
     }
-    /*
-        [HttpPut("auction/{id}", Name = "UpdateAuction")]a
-        public void UpdateAuction(string id, [FromBody] Auction auction)
+
+    [HttpPut("auction/{id}", Name = "UpdateAuction")]
+    public async Task<IActionResult> UpdateAuction(string id, [FromBody] Auction updatedauction)
+    {
+        var auction = await AuctionCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        if (auction == null)
         {
-            var auctionToUpdate = Collection.Find(x => x.Id == id).FirstOrDefault();
-
-            auctionToUpdate.AuctionItem = auction.AuctionItem;
-            auctionToUpdate.StartingPrice = auction.StartingPrice;
-            auctionToUpdate.CurrentBid = auction.CurrentBid;
-            auctionToUpdate.StartTime = auction.StartTime;
-            auctionToUpdate.EndTime = auction.EndTime;
-
-            Collection.ReplaceOne(x => x.Id == id, auctionToUpdate);
+            return NotFound();
         }
-    */
-    //___________________________________________________________________________________________________________________________________________________________//
-    // User methods
 
-    // [HttpGet("users", Name = "GetUsers")]
-    // public List<UserDTO> GetUsers()
-    // {
-    //     var usersCollection = GetUsersCollection(_config);
-    //     var userDocument = usersCollection.Find(new BsonDocument()).ToList();
+        updatedauction.Id = auction.Id;
 
-    //     _logger.LogInformation("GetUsers method called at {datetime}", DateTime.Now);
+        await AuctionCollection.ReplaceOneAsync(x => x.Id == id, updatedauction);
+        return Ok();
+    }
 
-    //     return userDocument.ToList();
-    // }
-
-    // [HttpGet("user/{id}", Name = "GetUserById")]
-    // public UserDTO GetUserById(string id)
-    // {
-    //     var usersCollection = GetUsersCollection(_config);
-    //     var userDocument = usersCollection.Find(new BsonDocument()).ToList();
-
-    //     _logger.LogInformation("GetUsers method called at {datetime}", DateTime.Now);
-
-    //     return userDocument.ToList().Find(x => x.UserId == id);
-    // }
-
-    // [HttpPut("user/{id}", Name = "UpdateUser")]
-    // public void UpdateUser(string id, [FromBody] UserDTO user)
-    // {
-    //     var usersCollection = GetUsersCollection(_config);
-    //     var userDocument = usersCollection.Find(new BsonDocument()).ToList();
-
-    //     var userToUpdate = userDocument.ToList().Find(x => x.UserId == id);
-
-    //     userToUpdate.Username = user.Username;
-    //     userToUpdate.Password = user.Password;
-    //     userToUpdate.Role = user.Role;
-    //     userToUpdate.Address = user.Address;
-    //     userToUpdate.Email = user.Email;
-    //     userToUpdate.Telephone = user.Telephone;
-
-    //     usersCollection.ReplaceOne(x => x.UserId == id, userToUpdate);
-    // }
-
-    // [HttpDelete("user/{id}", Name = "DeleteUser")]
-    // public void DeleteUser(string id)
-    // {
-    //     var usersCollection = GetUsersCollection(_config);
-    //     var userDocument = usersCollection.Find(new BsonDocument()).ToList();
-
-    //     var userToDelete = userDocument.ToList().Find(x => x.UserId == id);
-
-    //     usersCollection.DeleteOne(x => x.UserId == id);
-    // }
-
-
-
-    [HttpPost("user", Name = "CreateUser")]
-    public void CreateUser([FromBody] UserDTO user)
+    [HttpDelete("auction/{id}", Name = "DeleteAuction")]
+    public void DeleteAuction(string id)
     {
-        var usersCollection = GetUsersCollection(_config);
-        user.userID = ObjectId.GenerateNewId().ToString();
-        usersCollection.InsertOne(user);
-        _logger.LogInformation($"**********User{user.userID} has been created:**********");
+        AuctionCollection.DeleteOne(x => x.Id == id);
     }
 
 
-    /////////////////////////////////////////////////////////BID METHODS//////////////////////////////////////////////////////////////////////////////////////
-    //Gets the user id from the user collection
-    private string GetUserId(string bidderId)
-    {
-        var user = UsersCollection.Find(x => x.userID == bidderId).FirstOrDefault();
-        return user.userID;
-    }
-
-    //Gets the auction id from the auction collection
-    private string GetAuctionId(string auctionId)
-    {
-        var auction = AuctionCollection.Find(x => x.Id == auctionId).FirstOrDefault();
-        return auction.Id;
-    }
-
-
+    /////////////////////////////////////////////////////////BID METHOD//////////////////////////////////////////////////////////////////////////////////////
 
     [HttpPost("Bid", Name = "SendBid")]
     public void SendBid([FromBody] BidDTO bid)
     {
-        //var factory = new RabbitMQ.Client.ConnectionFactory() { HostName = "localhost" };
-        // var rabbitMQConnectionString = _config["RabbitMQ:ConnectionString"];
-        // var factory = new RabbitMQ.Client.ConnectionFactory() { Uri = new Uri(rabbitMQConnectionString) };
         var factory = new ConnectionFactory { HostName = _rabbitMQ };
 
         using var connection = factory.CreateConnection();
